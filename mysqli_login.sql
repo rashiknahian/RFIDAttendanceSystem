f90c6c6e99445aa05831fe7b967013d515160863
Rashik I Nahian <rashik@nahian.me>
2018-04-10 02:24:08 +0600

    database

--- mysqli_login.sql
+++ mysqli_login.sql
@@ -1,7 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Apr 09, 2018 at 10:23 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.8
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 --
--- Table structure for table `Attendance`
+-- Database: `RFIDAttendanceSystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
 --
 
 CREATE TABLE `attendance` (
@@ -9,7 +25,98 @@ CREATE TABLE `attendance` (
   `date` date NOT NULL,
   `starttime` time(6) NOT NULL,
   `endtime` time(6) NOT NULL,
-  `duration` time(6) 
+  `duration` time(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`username`, `date`, `starttime`, `endtime`, `duration`) VALUES
+('123', '2018-04-10', '00:00:00.000000', '01:22:00.000000', NULL),
+('123', '2018-04-10', '00:00:00.000000', '01:22:04.000000', NULL),
+('123', '2018-04-10', '01:53:51.000000', '01:53:51.000000', NULL),
+('123', '2018-04-10', '01:53:53.000000', '01:53:53.000000', NULL);
+
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `employeeData`
+--
+
+CREATE TABLE `employeeData` (
+  `id` int(11) NOT NULL,
+  `employeeId` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `designation` varchar(23) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(23) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `bloodGroup` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `image` longblob NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `employeeId` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `task` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2y$10$oTjXwA7Zsu.OOS.3nzGdWuyFnavAcopbrEi1aS9ogm9ZrVMF.P272');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employeeData`
+--
+ALTER TABLE `employeeData`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employeeData`
+--
+ALTER TABLE `employeeData`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
